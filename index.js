d75a371cf7a630907962c5e22e3372105f5f73bf
Sanambir Singh <sanambir123@gmail.com>
2022-11-01 16:06:04 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+
+app.listen(port, function(error) {
+    if (error) {
+        console.log('Error: ', error);
+        console.log(`Error in running the server: ${error}`);
+    }
+    console.log(`Server is up and running on port: ${port}`);
+})
