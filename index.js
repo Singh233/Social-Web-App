2cbe7155522e6341d252cba27e7b1bfac16e14e0
Sanambir Singh <sanambir123@gmail.com>
2022-11-02 19:42:23 +0530

    set up view engine

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const port = 8000;
 // use express router
 app.use('/', require('./routes'));
 
+// set up view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.listen(port, function(error) {
     if (error) {
         console.log('Error: ', error);
