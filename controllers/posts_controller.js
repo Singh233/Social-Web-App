4dfedd59898c568263138beea34d8f02f6849cb7
Sanambir Singh <sanambir123@gmail.com>
2022-11-02 19:39:36 +0530

    add posts route and controller

---
+++ controllers/posts_controller.js
@@ -0,0 +1,3 @@
+module.exports.posts = function(request, response) {
+    return response.end('<h1>Users posts</h1>');
+}
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const homeController = require('../controllers/home_controller');
+const postsController = require('../controllers/posts_controller');
 
 console.log('router loaded');
 
 
 router.get('/', homeController.home);
-router.use('/users', require('./users'))
-
+router.use('/users', require('./users'));
 
 // for any further routes, access from here
 // router.use('/routerName', require('./routerfile));
+router.get('/posts', postsController.posts);
 
 module.exports = router;
