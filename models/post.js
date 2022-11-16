ba932a256f9e72a64cef78e4daea3b4f03834ae6
Sanambir Singh <sanambir123@gmail.com>
2022-11-16 19:06:04 +0530

    create schema for comments

--- models/post.js
+++ models/post.js
@@ -3,6 +3,14 @@ const mongoose = require('mongoose');
 const postSchema = new mongoose.Schema({
     content: {type: String, required: true},
     user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    // include the array of ids of all comments in this post schema itself
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId, 
+            ref: 'comment'
+        }
+    ]
+
 }, {
     timestamps: true
 });
