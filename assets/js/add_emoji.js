ec778ab4b245f62bc7e00047f0ebf7541fb2239a
Sanambir Singh <sanambir123@gmail.com>
2022-12-04 22:44:49 +0530

    remove loging

--- assets/js/add_emoji.js
+++ assets/js/add_emoji.js
@@ -1,19 +1,21 @@
 
 {
     
-            const button = document.querySelector('.emoji-button');
-            const picker = new EmojiButton({
-                theme: 'dark'
-            });
-            
-            button.addEventListener('click', () => {
-                console.log("Clicked emoji", button);
-            
-                picker.togglePicker(button);
-            });
-            
-            picker.on('emoji', emoji => {
-                document.querySelector('#input-add-comment').value += emoji;
-            });
+    const picker = new EmojiButton({
+        theme: 'dark'
+    });
+    
+    const button = document.querySelector('.emoji-button');
+    
+    
+    button.addEventListener('click', () => {
+        console.log("Clicked emoji", button);
+    
+        picker.togglePicker(button);
+    });
+    
+    picker.on('emoji', emoji => {
+        document.querySelector('#input-add-comment').value += emoji;
+    });
         
 }
