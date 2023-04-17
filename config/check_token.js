6733109c03ef35e823371ed5728e4fdcadd9e95a
Sanambir Singh <Sanambir123@gmail.com>
2023-04-17 14:35:47 +0530

    Add new API endpoints and fix bugs

--- config/check_token.js
+++ config/check_token.js
@@ -1,12 +1,13 @@
 const User = require('../models/user');
 const jwt = require('jsonwebtoken');
+const env = require('../config/environment');
 
 module.exports.checkToken = function (request, response, next) {
     let token = request.headers.authorization.split(' ')[1];
     if (token == null) {
         return response.sendStatus(401);
     }
-    jwt.verify(token, 'codeial', function (error, decoded) {
+    jwt.verify(token, env.jwt_secret, function (error, decoded) {
         if (error) {
             return response.sendStatus(403);
         }
--- routes/api/v1/users.js
+++ routes/api/v1/users.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const { checkToken } = require('../../../config/check_token');
 const router = express.Router();
+const passport = require('passport');
 
 const usersApi = require('../../../controllers/api/v1/users_api');
+const { verifyGoogleToken } = require('../../../config/verify_google_token');
 
 router.post('/login', usersApi.createSession);
 router.get('/profile/:id', checkToken, usersApi.profile);
+router.post('/create', usersApi.create);
+
+router.get(
+    '/auth/google',
+    verifyGoogleToken,
+    usersApi.createGoogleSession,
+);
+
+// router.get(
+//     '/auth/google/callback',
+//     passport.authenticate('google', { failureRedirect: '/login' }),
+//     usersApi.createSession
+// );
 
 module.exports = router;
